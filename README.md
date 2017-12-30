fba51157e6bcddaf145733800325cd00cf450c25
fsantos91 <felipesan.cwb@gmail.com>
2017-12-30 14:32:04 -0200

    Subindo todos os arquivos

---
+++ README.md
@@ -0,0 +1,5 @@
+#Autor
+Nome= Felipe dos Santos
+Email= felipesan.cwb@gmail.com
+Site= felipesanblog.wordpress.com
+GitHub= fsantos91
---
+++ manutencaosis.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Script = Matutenção do Sistema
+# Info   = Fazer limpeza e atualização do sistema
+# Author = Felipe dos Santos
+# Email  = felipesan.cwb@gmail.com
+# Github = fsantos91
+
+echo "Atualizando repositórios..."
+sudo apt update
+echo ""
+echo ""
+echo "Instalando atualizações..."
+sudo apt upgrade -y
+echo ""
+echo ""
+echo "Limpando lixo residual do apt..."
+sudo apt autoclean -y
+echo ""
+echo ""
+echo "Limpando pacotes desnecessarios..."
+sudo apt autoremove -y
+echo ""
+echo ""
+echo "Limpando a lixeira"
+sudo rm -rf /home/$USER/.local/share/Trash/files/*
+echo ""
+echo ""
+echo "Limpando arquivos temporarios"
+sudo rm -rf /var/tmp/*
+echo ""
+echo ""
+echo "Reparando pacotes quebrados..."
+sudo dpkg --configure -a
+echo ""
+echo ""
+clear
+echo ""
+echo "Manutenção concluida!"
+sleep 4
+exit
